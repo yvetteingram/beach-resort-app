4e8f5912b3600739f6ddedb53f51c3e4d7840dc0
yvetteingram <yvetteingram@comcast.net>
2020-11-24 13:50:09 -0500

    revision

---
+++ react-projects/memegenerator/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Style from './style.css';
+import Header from './Header';
+import MemeGenerator from './MemeGenerator';
+
+
+function App() {
+  return (
+      <div>
+          <Header />
+          <MemeGenerator />
+      </div>
+  )
+}
+
+export default App
+
+
---
+++ react-projects/memegenerator/src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Functional component to display data
+function Header() {
+    return (
+      <header>
+            <img 
+                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                alt="" 
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
+
